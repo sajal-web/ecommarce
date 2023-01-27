712181bddfd267ede33743aa74e885be0f132c6a
Sajal <mahatosajal65@gmail.com>
2023-01-27 08:39:37 +0530

    login, registration activity added

--- app/src/main/java/com/ecommarce/cyberx/app/ecommarce/activity/MainActivity.java
+++ app/src/main/java/com/ecommarce/cyberx/app/ecommarce/activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.ecommarce.cyberx.app.ecommarce;
+package com.ecommarce.cyberx.app.ecommarce.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.ecommarce.cyberx.app.ecommarce.R;
 
 public class MainActivity extends AppCompatActivity {
 
